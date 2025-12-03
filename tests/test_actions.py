9febabdcf997d5d2c7b7010fc02d64fbaf2c1469
SpaceShaman <spaceshaman@tuta.io>
2025-12-03 09:58:03 +0100

    ✅ (test_actions.py): add test for new action 'action_with_result' to verify its functionality and ensure it returns the expected result

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -4,6 +4,7 @@ from socketapi.testclient import TestClient
 app = SocketAPI()
 
 action_called: int = 0
+action_with_result_called: int = 0
 
 
 @app.action("test_action")
@@ -12,9 +13,17 @@ async def action() -> None:
     action_called += 1
 
 
+@app.action("action_with_result")
+async def action_with_result(value: int) -> int:
+    global action_with_result_called
+    action_with_result_called += 1
+    return value * 2
+
+
 def test_trigger_action():
     global action_called
     client = TestClient(app)
+
     with client.websocket_connect("/") as websocket:
         websocket.send_json({"type": "action", "channel": "test_action"})
         response = websocket.receive_json()
@@ -24,3 +33,20 @@ def test_trigger_action():
             "status": "completed",
         }
         assert action_called == 1
+
+
+def test_trigger_action_with_result():
+    global action_with_result_called
+    client = TestClient(app)
+    with client.websocket_connect("/") as websocket:
+        websocket.send_json(
+            {"type": "action", "channel": "action_with_result", "data": {"value": 5}}
+        )
+        response = websocket.receive_json()
+        assert response == {
+            "type": "action",
+            "channel": "action_with_result",
+            "status": "completed",
+            "data": 10,
+        }
+        assert action_with_result_called == 1
