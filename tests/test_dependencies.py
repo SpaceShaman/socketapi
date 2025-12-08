be3136a92142e7ee6b53d390d70dbeddb6b05924
SpaceShaman <spaceshaman@tuta.io>
2025-12-08 17:25:44 +0100

    ✅ (test_dependencies.py): add test for action with nested dependency to ensure correct behavior

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Any
+from typing import Annotated
 
 from socketapi import Depends, SocketAPI
 from socketapi.testclient import TestClient
@@ -13,8 +13,21 @@ async def common_dependency(a: int, b: str) -> str:
 
 @app.action("action_one")
 async def action_one(
-    dep: Annotated[dict[str, Any], Depends(common_dependency)],
-) -> dict[str, Any]:
+    dep: Annotated[str, Depends(common_dependency)],
+) -> str:
+    return dep
+
+
+async def nested_dependency(
+    x: Annotated[str, Depends(common_dependency)],
+) -> dict[str, str]:
+    return {"x": x}
+
+
+@app.action("action_with_nested_dependency")
+async def action_with_nested_dependency(
+    dep: Annotated[dict[str, str], Depends(nested_dependency)],
+) -> dict[str, str]:
     return dep
 
 
@@ -38,4 +51,29 @@ def test_action_with_dependency():
             "channel": "action_one",
             "status": "completed",
             "data": "dependency result",
+        }
+
+
+def test_action_with_nested_dependency():
+    with client.websocket_connect("/") as websocket:
+        websocket.send_json(
+            {
+                "type": "action",
+                "channel": "action_with_nested_dependency",
+                "data": {
+                    "dep": {
+                        "x": {
+                            "a": 100,
+                            "b": "world",
+                        },
+                    },
+                },
+            }
+        )
+        response = websocket.receive_json()
+        assert response == {
+            "type": "action",
+            "channel": "action_with_nested_dependency",
+            "status": "completed",
+            "data": {"x": "dependency result"},
         }
